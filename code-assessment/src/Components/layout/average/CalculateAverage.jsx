b61550ba61e779547f232b5edb4771114c48ff92
hadeelsala7 <h.salah.technical@gmail.com>
2021-11-18 19:18:04 +0200

    Update StudentsList.js

---
+++ code-assessment/src/Components/layout/average/CalculateAverage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+function calculateAverage(allGrades){
+    let sum=0,
+    average =0,
+    i = 0;
+    for(i; i < allGrades.length;i++){
+        allGrades[i] = parseInt(allGrades[i]);
+        sum += allGrades[i];
+        average = sum / 8;
+    }
+    return average;
+}
+function CalculateAverage(props) {
+    return (
+        <div>
+           Average: {calculateAverage(props.props)}%
+        </div>
+    );
+}
+
+export default CalculateAverage;
